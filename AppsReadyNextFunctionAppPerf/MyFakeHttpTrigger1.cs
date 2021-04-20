00d1fc38d9e0c535d0fce35437565c984ae9046e
Gabe Shapiro <gabeshap@outlook.com>
2021-04-20 12:52:58 -0700

    re-adding custom class library

--- AppsReadyNextFunctionAppPerf/MyFakeHttpTrigger1.cs
+++ AppsReadyNextFunctionAppPerf/MyFakeHttpTrigger1.cs
@@ -17,7 +17,20 @@ namespace DevBootcampPrecompiledFunctions
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            //throw new System.IO.InvalidDataException("invalid data");
+            throw new System.IO.InvalidDataException("invalid data");
+
+            /*
+            try
+            {
+                throw new System.IO.InvalidDataException("invalid data");
+            }
+
+            catch (Exception ex)
+            {
+                log.LogInformation("invalid data exception occurred :( while passing in argument named invalid data.");
+            }
+            */
+
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             return (ActionResult)new OkObjectResult($"Well, at least it didn't return an HTTP error");
